bb654f3b6fab772b70a7d6b1f594772f639a188c
Emilio Casella <em.casella@reply.it>
2026-08-07 17:36:13 +0200

    feat: dashboard carica dati reali da OData

--- app/contratti/webapp/controller/Dashboard.controller.js
+++ app/contratti/webapp/controller/Dashboard.controller.js
@@ -4,32 +4,49 @@ sap.ui.define([
   "sap/ui/model/Filter",
   "sap/ui/model/FilterOperator",
   "../model/dashboardUtils",
+  "../model/aggregateCockpit",
   "../formatter"
-], function (BaseController, JSONModel, Filter, FilterOperator, dashboardUtils, mockCockpit, mockFornitori, formatter) {
+], function (BaseController, JSONModel, Filter, FilterOperator, dashboardUtils, aggregateCockpit, formatter) {
   "use strict";
 
   return BaseController.extend("com.reply.contrattiattivi.app.controller.Dashboard", {
     formatter: formatter,
 
     onInit: function () {
-      this.getView().setModel(new JSONModel(this._buildCockpitViewData()), "cockpit");
-      this.getView().setModel(new JSONModel({
-        lista: mockFornitori,
-        vista: "numero",
-        topFornitoriHtml: dashboardUtils.buildTopFornitoriHtml(mockFornitori, "numero")
-      }), "fornitori");
       this.getView().setModel(new JSONModel({ fornitoreAttivo: null }), "filtro");
+      this._caricaDati();
       this.getOwnerComponent().getRouter().getRoute("dashboard").attachPatternMatched(this._onRouteMatched, this);
     },
 
-    _buildCockpitViewData: function () {
-      return {
-        totaleContrattiAnno: mockCockpit.totaleContrattiAnno,
-        importoTotaleAnno: mockCockpit.importoTotaleAnno,
-        donutTipologiaHtml: dashboardUtils.buildDonutHtml(mockCockpit.tipologia),
-        donutSurveyHtml: dashboardUtils.buildDonutHtml(mockCockpit.survey),
-        trendHtml: dashboardUtils.buildTrendHtml(mockCockpit.trend)
-      };
+    _caricaDati: async function () {
+      var oModel = this.getView().getModel();
+      if (!oModel) {
+        this.getView().setModel(new JSONModel({ totaleContrattiAnno: 0, importoTotaleAnno: 0 }), "cockpit");
+        return;
+      }
+      var that = this;
+      try {
+        var aContrattiCtx = await oModel.bindList("/Contratto", { $filter: "stato ne 'ARCHIVIATO'" }).requestContexts(0, 999);
+        var aFornitoriCtx = await oModel.bindList("/Fornitore", {}).requestContexts(0, 999);
+        var contratti = aContrattiCtx.map(function (c) { return c.getObject(); });
+        var fornitori = aFornitoriCtx.map(function (f) { return f.getObject(); });
+        var ui = aggregateCockpit({ contratti: contratti, fornitori: fornitori });
+        var sTopFornitoriHtml = dashboardUtils.buildTopFornitoriHtml(ui.topFornitori, "fatturato");
+        that.getView().setModel(new JSONModel({
+          totaleContrattiAnno: ui.totaleContratti,
+          importoTotaleAnno: ui.importoTotaleAnno,
+          donutTipologiaHtml: dashboardUtils.buildDonutHtml(ui.donutTipologia),
+          donutSurveyHtml: dashboardUtils.buildDonutHtml(ui.donutSurvey),
+          trendHtml: dashboardUtils.buildTrendHtml(ui.trend)
+        }), "cockpit");
+        that.getView().setModel(new JSONModel({
+          lista: ui.topFornitori,
+          vista: "importi",
+          topFornitoriHtml: sTopFornitoriHtml
+        }), "fornitori");
+      } catch (err) {
+        console.error("Dashboard load error", err);
+      }
     },
 
     _onRouteMatched: function (oEvent) {
