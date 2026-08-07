b0b786dfc07413a36a1fc52504c3bdba24bb2a26
Emilio Casella <em.casella@reply.it>
2026-08-07 13:00:53 +0200

    feat(contratti UI5): rimuovi tab reportistica da dashboard, apri contratto da dettaglio

--- app/contratti/webapp/controller/Dashboard.controller.js
+++ app/contratti/webapp/controller/Dashboard.controller.js
@@ -21,10 +21,6 @@ sap.ui.define([
         topFornitoriHtml: dashboardUtils.buildTopFornitoriHtml(mockFornitori, "numero")
       }), "fornitori");
       this.getView().setModel(new JSONModel({ fornitoreAttivo: null }), "filtro");
-      this.getView().setModel(new JSONModel([]), "integrazione");
-
-      this.getOwnerComponent().getRouter().getRoute("dashboard").attachPatternMatched(this._onRouteMatched, this);
-      this._loadIntegrazioneContratti();
     },
 
     _buildCockpitViewData: function () {
@@ -69,42 +65,14 @@ sap.ui.define([
       oModel.setProperty("/topFornitoriHtml", dashboardUtils.buildTopFornitoriHtml(aLista, sVista));
     },
 
-    onFornitoreVendorMeshPress: function (oEvent) {
-      var sNome = oEvent.getSource().getBindingContext("fornitori").getProperty("nome");
-      this.getOwnerComponent().getRouter().navTo("dashboard", { fornitore: sNome });
-    },
-
-    onApriContrattoIntegrazione: function (oEvent) {
-      var sID = oEvent.getSource().getBindingContext("integrazione").getProperty("ultimoContrattoID");
+    onApriContrattoDettaglio: function (oEvent) {
+      var sID = oEvent.getSource().getBindingContext().getProperty("ID");
       if (!sID) return;
       this.getOwnerComponent().getRouter().navTo("detail", { id: encodeURIComponent(sID) });
     },
 
     onNavBack: function () {
       this.getOwnerComponent().getRouter().navTo("main");
-    },
-
-    _loadIntegrazioneContratti: async function () {
-      var oModel = this.getOwnerComponent().getModel();
-      var sBase = oModel.getServiceUrl();
-      var aLista = this.getView().getModel("fornitori").getProperty("/lista");
-      try {
-        var oResp = await fetch(sBase + "Contratto?$filter=stato ne 'ARCHIVIATO'&$select=ID,intestatario,importo,dataStipula&$orderby=dataStipula desc");
-        var oJson = await oResp.json();
-        var aContratti = oJson.value || [];
-        var aRisultati = aLista.map(function (f) {
-          var aMatch = aContratti.filter(function (c) { return dashboardUtils.matchFornitore(f.nome, c.intestatario); });
-          if (!aMatch.length) return null;
-          return {
-            nome: f.nome,
-            numeroContratti: aMatch.length,
-            importoTotale: aMatch.reduce(function (n, c) { return n + (c.importo || 0); }, 0),
-            ultimaDataStipula: aMatch[0].dataStipula,
-            ultimoContrattoID: aMatch[0].ID
-          };
-        }).filter(function (r) { return !!r; });
-        this.getView().setModel(new JSONModel(aRisultati), "integrazione");
-      } catch (e) { /* rete non disponibile: tabella integrazione resta vuota */ }
     }
   });
 });
